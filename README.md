09218a55eb7289d0f2049e8d8f5ac1e3071f641a
Larry Hitchon <larryhitchon@gmail.com>
2012-01-10 11:16:52 +0100

    validate the JSON before allowing form submission

---
+++ README.md
@@ -0,0 +1,17 @@
+# MongoDB Browser Example App
+
+Example Sinatra application to browser collections and documents in a MongoDB.
+
+Supports the following:
+
+* Create collections
+* Drop empty collections
+* Create document in a collection
+* Edit document 
+* Delete document
+* Validates JSON in browser before posting document add or update
+
+The following are left as the proverbial exercise for the reader
+
+* Paging for long lists of collections and documents
+* Searching
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
+var json_error = function(s) {
+  var error = null;
+  try {
+    JSON.parse(s);
+  } 
+  catch(err) {
+    error = err.message;
+  }
+  return error;
+}
+
 $(document).ready(function(){
   $('a.popup').click(function(e){
     var dom_id = $(this).attr('data');
@@ -10,4 +21,15 @@ $(document).ready(function(){
   $('div.popup a.close').click(function(e){
     $(this).closest('div.popup').fadeOut();
   });
+  $('#edit_document input[type="submit"],#new_document input[type="submit"]').click(function(e){
+    var error = json_error($('#document').val());
+    if ( error ) {
+      console.log(error);
+      $('#json_error').html(error);
+      $('#json_error').addClass('error');
+      e.preventDefault();
+    } else {
+      $('#json_error').removeClass('error');
+    }
+  })
 });
